9ab19c65b76b1363507fdd21a9a7011ba1fe7526
Eugene Beletskiy <ebeletskiy@gmail.com>
2015-04-09 20:30:07 +0200

    Interactor impl / tests

--- app/src/main/java/mobile/goeuro/ebeletskiy/goeuromobiletest/ui/fragments/travel/TravelInteractor.java
+++ app/src/main/java/mobile/goeuro/ebeletskiy/goeuromobiletest/ui/fragments/travel/TravelInteractor.java
@@ -1,11 +1,8 @@
 package mobile.goeuro.ebeletskiy.goeuromobiletest.ui.fragments.travel;
 
-import java.util.List;
-import mobile.goeuro.ebeletskiy.goeuromobiletest.data.api.model.DestinationPoint;
 import org.jetbrains.annotations.NotNull;
 
 public interface TravelInteractor {
 
-  List<DestinationPoint> getDestinationPoints(@NotNull String city);
-
+  void getDestinationPoints(@NotNull String city);
 }
