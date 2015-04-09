e65170150218a7241afbf076451285983d399079
Eugene Beletskiy <ebeletskiy@gmail.com>
2015-04-09 15:50:56 +0200

    MainActivity's layout updated to hold a Fragment;

---
+++ app/src/main/java/mobile/goeuro/ebeletskiy/goeuromobiletest/ui/MainActivity.java
@@ -0,0 +1,22 @@
+package mobile.goeuro.ebeletskiy.goeuromobiletest.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import mobile.goeuro.ebeletskiy.goeuromobiletest.R;
+import mobile.goeuro.ebeletskiy.goeuromobiletest.ui.fragments.travel.TravelFragment;
+
+public class MainActivity extends ActionBarActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    if (savedInstanceState == null) {
+      getSupportFragmentManager()
+          .beginTransaction()
+          .add(R.id.rootview, new TravelFragment())
+          .commit();
+    }
+  }
+}
