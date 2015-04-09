3f1e7fe2796b46dd3b85c44c488cc8b75260cff7
Eugene Beletskiy <ebeletskiy@gmail.com>
2015-04-09 15:51:28 +0200

    InjectableFragment.java introduced.

---
+++ app/src/main/java/mobile/goeuro/ebeletskiy/goeuromobiletest/ui/base/InjectableFragment.java
@@ -0,0 +1,30 @@
+package mobile.goeuro.ebeletskiy.goeuromobiletest.ui.base;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import dagger.ObjectGraph;
+import mobile.goeuro.ebeletskiy.goeuromobiletest.App;
+
+@SuppressWarnings("AbstractClassExtendsConcreteClass")
+public abstract class InjectableFragment extends Fragment {
+
+  protected ObjectGraph scopedGraph;
+
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    scopedGraph = ((App) getActivity().getApplication()).createScopedGraph(getModules());
+    scopedGraph.inject(this);
+  }
+
+  @Override public void onDestroy() {
+    super.onDestroy();
+    scopedGraph = null;
+  }
+
+  public abstract Object getModules();
+
+  public ObjectGraph getScopedGraph() {
+    return scopedGraph;
+  }
+}
