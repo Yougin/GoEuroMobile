f5ffa0ae0adbe43f855a39ffb03398add30af4fc
Eugene Beletskiy <ebeletskiy@gmail.com>
2015-04-09 15:08:27 +0200

    GetDestinationPointsJob.java: get city as parameter during object construction; More tests.

--- app/src/main/java/mobile/goeuro/ebeletskiy/goeuromobiletest/jobs/GetDestinationPointsJob.java
+++ app/src/main/java/mobile/goeuro/ebeletskiy/goeuromobiletest/jobs/GetDestinationPointsJob.java
@@ -1,11 +1,15 @@
 package mobile.goeuro.ebeletskiy.goeuromobiletest.jobs;
 
+import com.google.common.base.Preconditions;
 import com.path.android.jobqueue.Params;
 import de.greenrobot.event.EventBus;
+import java.util.List;
 import javax.inject.Inject;
 import mobile.goeuro.ebeletskiy.goeuromobiletest.data.api.WebService;
+import mobile.goeuro.ebeletskiy.goeuromobiletest.data.api.model.DestinationPoint;
 import mobile.goeuro.ebeletskiy.goeuromobiletest.events.DestinationPointsEvents;
 import mobile.goeuro.ebeletskiy.goeuromobiletest.utils.language.LanguageProvider;
+import org.jetbrains.annotations.NotNull;
 
 public class GetDestinationPointsJob extends BaseJob {
 
@@ -17,8 +21,13 @@ public class GetDestinationPointsJob extends BaseJob {
   @Inject DestinationPointsEvents.SuccessEvent successEvent;
   @Inject DestinationPointsEvents.FailEvent failEvent;
 
-  public GetDestinationPointsJob() {
+  private String city;
+
+  public GetDestinationPointsJob(@NotNull String city) {
     super(new Params(Priority.NORMAL).requireNetwork().persist());
+
+    this.city =
+        Preconditions.checkNotNull(city, "city can't be null, make sure you don't pass null here");
   }
 
   @Override public void onAdded() {
@@ -26,11 +35,12 @@ public class GetDestinationPointsJob extends BaseJob {
   }
 
   @Override public void onRun() throws Throwable {
-
+    List<DestinationPoint> destinationPoints =
+        webService.getDestinationPoints(languageProvider.getUserLanguage(), city);
   }
 
   @Override protected void onCancel() {
-
+    bus.postSticky(failEvent);
   }
 
   @Override protected boolean shouldReRunOnThrowable(Throwable throwable) {
--- app/src/test/java/mobile/goeuro/ebeletskiy/goeuromobiletest/jobs/GetDestinationPointsJobTest.java
+++ app/src/test/java/mobile/goeuro/ebeletskiy/goeuromobiletest/jobs/GetDestinationPointsJobTest.java
@@ -5,19 +5,29 @@ import dagger.ObjectGraph;
 import dagger.Provides;
 import de.greenrobot.event.EventBus;
 import mobile.goeuro.ebeletskiy.goeuromobiletest.data.api.WebService;
+import mobile.goeuro.ebeletskiy.goeuromobiletest.data.api.error.NetworkConnectionException;
 import mobile.goeuro.ebeletskiy.goeuromobiletest.events.DestinationPointsEvents;
 import mobile.goeuro.ebeletskiy.goeuromobiletest.utils.language.LanguageProvider;
 import org.junit.Before;
 import org.junit.Test;
 
 import static mobile.goeuro.ebeletskiy.goeuromobiletest.jobs.GetDestinationPointsJobTest.TestModule.bus;
+import static mobile.goeuro.ebeletskiy.goeuromobiletest.jobs.GetDestinationPointsJobTest.TestModule.failEvent;
+import static mobile.goeuro.ebeletskiy.goeuromobiletest.jobs.GetDestinationPointsJobTest.TestModule.languageProvider;
 import static mobile.goeuro.ebeletskiy.goeuromobiletest.jobs.GetDestinationPointsJobTest.TestModule.startedEvent;
 
 import static mobile.goeuro.ebeletskiy.goeuromobiletest.jobs.GetDestinationPointsJobTest.TestModule.webService;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class GetDestinationPointsJobTest {
+
+  public static final String CITY_PARAMETER = "city";
 
   private GetDestinationPointsJob job;
 
@@ -64,7 +74,7 @@ public class GetDestinationPointsJobTest {
   }
 
   @Before public void setUp() throws Exception {
-    job = new GetDestinationPointsJob();
+    job = new GetDestinationPointsJob(CITY_PARAMETER);
     ObjectGraph.create(new TestModule()).inject(job);
   }
 
@@ -74,14 +84,44 @@ public class GetDestinationPointsJobTest {
     verify(bus).post(startedEvent);
   }
 
-  //@Test public void should_invoke_getDestinationPoints_for_webservice_when_job_is_running()
-  //    throws Exception {
-  //  try {
-  //    job.onRun();
-  //  } catch (Throwable throwable) {
-  //    // catch silently
-  //  }
-  //
-  //  verify(webService).getDestinationPoints()
-  //}
+  @Test public void should_invoke_getDestinationPoints_for_webservice_when_job_is_running()
+      throws Exception, NetworkConnectionException {
+    jobOnRun();
+
+    verify(webService).getDestinationPoints(anyString(), anyString());
+  }
+
+  @Test public void should_invoke_getLanguage_for_language_provider_when_job_is_running()
+      throws Exception {
+    jobOnRun();
+
+    verify(languageProvider).getUserLanguage();
+  }
+
+  @Test public void should_pass_exactly_the_city_the_job_was_initialized_with()
+      throws Exception, NetworkConnectionException {
+    jobOnRun();
+
+    verify(webService).getDestinationPoints(anyString(), eq(CITY_PARAMETER));
+  }
+
+  @Test public void should_fire_failure_event_if_job_canceled() throws Exception {
+    job.onCancel();
+
+    verify(bus).postSticky(failEvent);
+  }
+
+  @Test public void should_the_job_be_repeated_in_case_of_failure() throws Exception {
+    boolean isRepeatable = job.shouldReRunOnThrowable(new RuntimeException());
+
+    assertThat(isRepeatable, is(true));
+  }
+
+  private void jobOnRun() {
+    try {
+      job.onRun();
+    } catch (Throwable throwable) {
+      // catch silently
+    }
+  }
 }
